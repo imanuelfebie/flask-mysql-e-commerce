df4a73c49a94dfdb1fe7af84140190677c283c17
Imanuel Febie <imanuel.febie@binus.ac.id>
2020-01-01 21:05:35 +0700

    Added register page

---
+++ ecommerce/forms.py
@@ -0,0 +1,28 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+class UserRegistrationForm(FlaskForm):
+    '''Fields for new user registration'''
+    email = StringField('email', validators=[
+        DataRequired(), 
+        Email()])
+    firstname = StringField('firstname', validators=[DataRequired()])
+    lastname = StringField('lastname', validators=[DataRequired()])
+    password1 = PasswordField('password', validators=[DataRequired()]) 
+    password2 = PasswordField('confirm password', validators=[
+        DataRequired(),
+        EqualTo('password1')])
+    submit = SubmitField('Register')
+
+    # Need to add username validation - Checking wether user already exist or not
+    # both username and email
+
+
+class StoreRegistrationForm(FlaskForm):
+    '''Form to create a store'''
+    name = StringField('store name', validators=[DataRequired()])
+    description = TextAreaField('description')
+
+    
--- ecommerce/routes.py
+++ ecommerce/routes.py
@@ -1,6 +1,17 @@
-from flask import render_template
+from flask import render_template, redirect, url_for, request
+from .forms import UserRegistrationForm, StoreRegistrationForm
 from ecommerce import app
+
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/register')
+def register_user():
+    form = UserRegistrationForm() # instance of the UserRegistrationForm
+    
+    # if form.validate_on_submit():
+
+    return render_template('register.html', form=form)
