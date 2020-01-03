4191f14dff7e3faf43112b231375885fdfcf7418
Imanuel Febie <imanuel.febie@binus.ac.id>
2020-01-03 14:43:47 +0700

    Installed pyyaml and configured MySQL database

--- ecommerce/__init__.py
+++ ecommerce/__init__.py
@@ -1,6 +1,21 @@
 from flask import Flask
+from flask_mysqldb import MySQL
+import yaml
 
-app = Flask(__name__)
+# Flask object
+app = Flask(__name__) 
+
+# Secret key
 app.config['SECRET_KEY'] = '12345678'
+
+# Database config
+db_config = yaml.load(open('db.yaml'))
+
+app.config['MYSQL_HOST'] = db_config['host']
+app.config['MYSQL_USER'] = db_config['username']
+app.config['MYSQL_PASSWORD'] = db_config['password']
+app.config['MYSQL_DB'] = db_config['database']
+
+db = MySQL(app) # MySQL instance and passing the Flask object as an argument
 
 from ecommerce import routes
