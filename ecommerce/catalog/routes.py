d6288454e9c879f721b8e5d741425dea6b2095bc
Ikhsan M <ikhsanmaulanapribadi19@gmail.com>
2020-01-08 01:20:27 +0700

    add product page fixed

--- ecommerce/catalog/routes.py
+++ ecommerce/catalog/routes.py
@@ -36,24 +36,27 @@ def product_list():
 def product_create():
     form = ProductCreateForm()
     category_list = Category.objects_all()
+    cvalue = ""
 
-    if form.validate_on_submit():
+    if request.method == "POST":
+        cvalue = request.form['cvalue']
+
+    if form.validate_on_submit:
+
         product = Product(
             form.name.data,
             form.description.data,
             form.stock.data,
             form.price.data,
             form.available.data,
-            form.category.data
+            cvalue
             )
         product.create_object()
 
         print("Product added")
     else:
-        print(form.name.data)
-        print("fail")
-        print(form.category.data)
-        print(request.args.get('category'))
+        print("Adding product failed")
+
 
     return render_template('product_create.html', form=form, category_list=category_list)
 
