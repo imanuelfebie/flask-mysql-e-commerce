290afafdc96d6609970118f02fc6cfb9ed1d4f23
Imanuel Febie <imanuel.febie@binus.ac.id>
2020-01-08 02:50:24 +0700

    A working user session

--- ecommerce/users/routes.py
+++ ecommerce/users/routes.py
@@ -1,56 +1,71 @@
-from flask import Blueprint, render_template, session, redirect, url_for, flash
+from flask import Blueprint, render_template, session, redirect, url_for, flash, g
+from flask_login import login_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from .forms import UserRegistrationForm, AdminLogin, StoreRegistrationForm, UserLoginForm
 from ecommerce.users.models import User, Store
-from ecommerce import mysql, login_manager
+#from ecommerce import mysql, login_manager
 from ecommerce.catalog.models import Basket,Total
 
 users = Blueprint('users', __name__)
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.get(user_id)
+#@login_manager.user_loader
+#def load_user(user_id):
+#    return User.get(user_id)
 
-@users.route('/admin-login', methods=['GET', 'POST'])
-def admin_login():
-    '''Admin login page'''
-    form = AdminLogin()
+@users.route('/getsession')
+def get_session():
+    if 'user' in session:
+        return session['user']
+    return 'Anynomous'
 
-    return render_template('admin_login.html', form=form)
+@users.before_request
+def before_request():
+    g.user = None
+    if 'user' in session:
+        g.user = session['user']
 
 @users.route('/login', methods=['GET', 'POST'])
 def user_login():
-    '''User login'''
-    form = UserLoginForm()
-    error = None
 
-    if form.validate_on_submit():
-        user = User.filter_by(form.email.data)
-        #print(user['password'])
-        if user is None:
-            #print(str(user['email'])) 
-            #hashed_password = user['password']
-            password = form.password.data
-            #print(password)
-            #print(hashed_password)
-            #print(check_password_hash(hashed_password, password))
-            print('user doesn\'t exist')
-            error = 'User doesn\'t exist'
-        elif not check_password_hash(user['password'], form.password.data):
-            print("password is incorrent")
-            error = 'Incorrect password'
-        else:   
-            return redirect(url_for('users.user_dashboard'))
+    if not g.user:
+        '''User login'''
+        form = UserLoginForm()
+        error = None
 
-        flash(error)
+        if form.validate_on_submit():
+            user = User.filter_by(form.email.data)
+            session.pop('user', None)
+            if user is None:
+                error = 'User doesn\'t exist'
+            elif not check_password_hash(user['password'], form.password.data):
+                error = 'Please insert correct username and password'
+            else:
+                #login_user(user)
+                # print(session['user'])
+                session['user'] = user
+                #user['is_authenticated'] = True
+                return redirect(url_for('users.user_dashboard'))
 
-    return render_template('login.html', form=form)
+            flash(error)
+
+        return render_template('login.html', form=form)
+    else:
+        return redirect(url_for('users.user_dashboard'))
+
+
+#session.pop('user', None)
+
+@users.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect(url_for('main.index'))
 
 
 @users.route('/register', methods=['POST', 'GET'])
 def register():
     '''User registration page'''
     form = UserRegistrationForm()
+    message = None
 
     if form.validate_on_submit():
         user = User(
@@ -60,7 +75,7 @@ def register():
                 generate_password_hash(form.password1.data)
                 )
         user.create_object()   
-
+        flash('Awesome! You just created an account and can now login')
         return redirect(url_for('users.user_login'))
 
     return render_template('register.html', form=form)
@@ -68,9 +83,10 @@ def register():
 @users.route('/dashboard')
 def user_dashboard():
     '''User account dashboard'''
-    user = User.get(11)    
-
-    return render_template('user_dashboard.html', user=user)
+    if not g.user:
+        return redirect(url_for('users.user_login'))
+    return render_template('user_dashboard.html')
+    
 
 @users.route('/basket_list')
 def basket_list():
