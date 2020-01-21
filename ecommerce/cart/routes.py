b425b60cb7d0f1e2992048784f4524c9ca14066a
Imanuel Febie <imanuel.febie@binus.ac.id>
2020-01-21 17:24:45 +0700

    Changed color back to green and fixed some routing issues

--- ecommerce/cart/routes.py
+++ ecommerce/cart/routes.py
@@ -1,7 +1,13 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, session, g
 from ecommerce.cart.cart import Cart
 
 cart = Blueprint('cart', __name__)
+
+@cart.before_request
+def before_request():
+    g.user = None
+    if 'user' in session:
+        g.user = session['user']
 
 @cart.route('/cart')
 def shopping_cart():
